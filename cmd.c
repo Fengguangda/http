8e33ceaba9ee7d32c37d278a28d12b2ffe37c560
Sunil Nimmagadda <sunil@nimmagadda.net>
2018-01-20 20:09:46 +0530

    Start with a prompt and line history.

--- cmd.c
+++ cmd.c
@@ -14,9 +14,53 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <err.h>
+#include <histedit.h>
+#include <string.h>
+#include <stdlib.h>
+
 #include "http.h"
+
+static char	*prompt(void);
+
+static char *
+prompt(void)
+{
+	return "ftp> ";
+}
 
 void
 cmd(const char *host, const char *port)
 {
+	HistEvent	 hev;
+	EditLine	*el;
+	History		*hist;
+	const char	*line;
+	int		 count;
+
+	if ((el = el_init(getprogname(), stdin, stdout, stderr)) == NULL)
+		err(1, "couldn't initialise editline");
+
+	if ((hist = history_init()) == NULL)
+		err(1, "couldn't initialise editline history");
+
+	history(hist, &hev, H_SETSIZE, 100);
+	el_set(el, EL_HIST, history, hist);
+	el_set(el, EL_PROMPT, prompt);
+	el_set(el, EL_EDITOR, "emacs");
+	el_set(el, EL_TERMINAL, NULL);
+	el_set(el, EL_SIGNAL, 1);
+	el_source(el, NULL);
+
+	for (;;) {
+		if ((line = el_gets(el, &count)) == NULL || count <= 0) {
+			fprintf(stderr, "\n");
+			break;
+		}
+
+		if (strlen(line) > 1)
+			history(hist, &hev, H_ENTER, line);
+	}
+
+	el_end(el);
 }
